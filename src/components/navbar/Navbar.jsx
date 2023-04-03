14cceecd0e0dec7c39ddcb65d06d148a8befa65a
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-04-03 15:06:19 +0300

    Update Navbar.jsx
    and navbar.css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -20,6 +20,13 @@ const Navbar = () => {
           <p><a href="#blog">Library</a></p>
         </div>
       </div>
+      <div className="gpt3__navbar-sign"> 
+      <p>Sign in</p>
+      <button type="button">Sign up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        
+      </div>
     </div>
   )
 }
