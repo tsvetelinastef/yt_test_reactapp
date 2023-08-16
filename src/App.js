36e97795ed57ec64dab74b811a2eb7744ae88455
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-08-16 13:16:01 +0300

    modifications

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ const App = () => {
     return (
         <div className="App" >
             <div className="gradient__bg">
-                <Navbar />
-                <Header />
+                <Navbar /> 
+                <Header /> 
             </div>
             <Brand /> 
             <WhatGPT3 /> 
