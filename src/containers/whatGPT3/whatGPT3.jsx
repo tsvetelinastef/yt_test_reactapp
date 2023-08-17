fde341e62cb0fba7056775b72c7b51977c2d3227
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-08-17 22:41:04 +0300

    modifications

--- src/containers/whatGPT3/whatGPT3.jsx
+++ src/containers/whatGPT3/whatGPT3.jsx
@@ -3,8 +3,22 @@ import './whatGPT3.css';
 
 const whatGPT3 = () => {
   return (
-    <div>
-      whatGPT3
+    // <div>
+    //   whatGPT3
+    // </div>
+    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient-text">The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
     </div>
   )
 }
