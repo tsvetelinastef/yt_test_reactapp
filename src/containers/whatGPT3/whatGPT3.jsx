5e24f35fff00ca63bc47ec87421bbb87d788b5f5
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-08-18 16:00:55 +0300

    modifications

--- src/containers/whatGPT3/whatGPT3.jsx
+++ src/containers/whatGPT3/whatGPT3.jsx
@@ -9,7 +9,7 @@ const whatGPT3 = () => {
     // </div>
     <div className="gpt3__whatgpt3 section__margin" id="whpt3">
       <div className="gpt3__whatgpt3-feature">
-        <Feature />
+        <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
       </div>
       <div className="gpt3__whatgpt3-heading">
         <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
