cc5305f2c55d49814fef6c4ab2a43bfca0b9d830
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-08-22 00:45:29 +0300

    Added copyright notice to footer... we have the text but not styled

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -38,6 +38,10 @@ const Footer = () => {
           <p>info@payme.net</p> 
         </div>
       </div>
+
+      <div className="gpt3__footer-copyright">
+        <p>© 2021 GPT-3. All rights reserved.</p>
+      </div>
     </div>
   )
 }
