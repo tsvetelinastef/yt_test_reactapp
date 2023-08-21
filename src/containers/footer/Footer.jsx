9af9c90e76b070c3ed2d41bc3b48e18ee15b7787
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-08-22 00:05:47 +0300

    Added company and contact information to footer.

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -25,6 +25,18 @@ const Footer = () => {
           <p>Counters</p>
           <p>Contact</p> 
         </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p> 
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get in touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085-132567</p>
+          <p>info@payme.net</p> 
+        </div>
       </div>
     </div>
   )
