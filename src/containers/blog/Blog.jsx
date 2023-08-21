8645a7ce788ca772d129362e061929082f9e4719
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2023-08-21 14:06:26 +0300

    Added article.css and blog.css.

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -7,7 +7,7 @@ const Blog = () => {
   return (
     <div className="gpt3__blog section__padding" id="blog">
       <div className="gpt3__blog-heading">
-        <h1 className="gradient__text">A lot is happening, We are blogging about it.</h1>
+        <h1 className="gradient__text">A lot is happening. We are blogging about it.</h1>
       </div>
       <div className="gpt3__blog-container"> 
         <div className="gpt3__blog-container_groupA"> 
